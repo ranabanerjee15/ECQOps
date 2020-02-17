faa1607ce7f0df76ca157d178eae390ace6f70f7
Rana Banerjee <rana.b4523@gmail.com>
2020-02-17 15:17:33 +1000

    initial commit

---
+++ ECQOps.psm1
@@ -0,0 +1,19 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2020 v5.7.172
+	 Created on:   	17/02/2020 2:56 PM
+	 Created by:   	sysda242
+	 Organization: 	
+	 Filename:     	ECQOps.psm1
+	-------------------------------------------------------------------------
+	 Module Name: ECQOps
+	===========================================================================
+#>
+
+
+
+function Write-HelloWorld
+{
+	Write-Host "Hello World"
+}
+
